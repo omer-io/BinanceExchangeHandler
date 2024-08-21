ce7a29393ae754d9b1f04937c790355e46781e88
omer-io <omer.jamil@rakurai.io>
2024-08-22 01:48:27 +0500

    fetching data of all 3 endpoints using threads

--- app/main.cpp
+++ app/main.cpp
@@ -4,26 +4,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <thread>
 
-#include "https.h"
+void spotData(std::string, std::string);
+void usdFutureData(std::string, std::string);
+void coinFutureData(std::string, std::string);
 
 // global variables
-std::string spot_exchange_info_uri;
-std::string usd_futures_exchange_info_uri;
-std::string coin_futures_exchange_info_uri;
-
-std::string spot_exchange_info_base_uri;
-std::string usd_futures_exchange_info_base_uri;
-std::string coin_futures_exchange_info_base_uri;
+std::string spot_exchange_info_base_uri, usd_futures_exchange_info_base_uri, coin_futures_exchange_info_base_uri;
+std::string spot_exchange_info_uri, usd_futures_exchange_info_uri, coin_futures_exchange_info_uri;
 int request_interval;
 
-rapidjson::Document doc;
-void readConfig(std::string configFile) {
+void readConfig(std::string configFile, rapidjson::Document &doc) {
 
     FILE* fp = fopen(configFile.c_str(), "r"); 
-    if (!fp) {
-        throw std::runtime_error("Could not open config file: " + configFile);
-    }
+    if (!fp) { 
+        std::cerr << "Error: unable to open file" << std::endl; 
+    } 
 
     char buffer[65536];
     rapidjson::FileReadStream is(fp, buffer, sizeof(buffer));
@@ -42,22 +39,16 @@ void readConfig(std::string configFile) {
     fclose(fp); 
 
 }
-struct symbolInfo{
-    std::string symbol;
-    std::string quoteAsset;
-    std::string status;
-    std::string tickSize;
-    std::string stepSize;
-};
 
-class exchangeInfo{
-    private:
-        std::vector<symbolInfo> symbols;
-};
+// class exchangeInfo{
+//     private:
+//         std::vector<symbolInfo> symbols;
+// };
 
 int main() {
 
-    readConfig("/home/omer/training/BinanceExchangeHandler/config.json");
+    rapidjson::Document doc;
+    readConfig("/home/omer/training/BinanceExchangeHandler/config.json", doc);
 
     std::cout << "Spot Exchange Info base URI: " << spot_exchange_info_base_uri << std::endl;
     std::cout << "USD Futures Exchange Info base URI: " << usd_futures_exchange_info_base_uri << std::endl;
@@ -67,78 +58,13 @@ int main() {
     std::cout << "Coin Futures Exchange Info URI: " << coin_futures_exchange_info_uri << std::endl;
     std::cout << "Request Interval: " << request_interval << " seconds" << std::endl;
 
-
-    auto const host = doc["exchange_base_url"]["spot_exchange_info_base_uri"].GetString();
-    auto const port = "443";
-    auto const target = doc["exchange_endpoints"]["spot_exchange_info_uri"].GetString();
-    int version = 11;
-    
-    // The io_context is required for all I/O
-    net::io_context ioc;
-
-    // The SSL context is required, and holds certificates
-    ssl::context ctx{ssl::context::tlsv12_client};
-
-    // This holds the root certificate used for verification
-    load_root_certificates(ctx);
-
-    // Verify the remote server's certificate
-    ctx.set_verify_mode(ssl::verify_peer);
-
-    // Launch the asynchronous operation
-    // The session is constructed with a strand to
-    // ensure that handlers do not execute concurrently.
-    std::make_shared<session>(
-        net::make_strand(ioc),
-        ctx
-        )->run(host, port, target, version);
-
-    // Run the I/O service. The call will return when
-    // the get operation is complete.
-    ioc.run();
-
-    http::response<http::string_body> spotExchange;
-    spotExchange = returnResponse();
-    std::cout << spotExchange << std::endl;
-
-    rapidjson::Document fullData;
-    fullData.Parse(spotExchange.body().c_str());
-
-    if (!fullData.IsObject() || !fullData.HasMember("symbols") || !fullData["symbols"].IsArray()) {
-        std::cerr << "Invalid JSON format or missing symbols array." << std::endl;
-        return 0;
-    }
-
-    const auto& symbolsArray = fullData["symbols"];
-    std::vector<symbolInfo> symbols;
-
-    for (const auto& symbol : symbolsArray.GetArray()) {
-        symbolInfo info;
-        info.symbol = symbol["symbol"].GetString();
-        info.quoteAsset = symbol["quoteAsset"].GetString();
-        info.status = symbol["status"].GetString();
-
-        for (const auto& filter : symbol["filters"].GetArray()) {
-            std::string filterType = filter["filterType"].GetString();
-            if (filterType == "PRICE_FILTER") {
-                info.tickSize = filter["tickSize"].GetString();
-            } else if (filterType == "LOT_SIZE") {
-                info.stepSize = filter["stepSize"].GetString();
-            }
-        }
-
-        symbols.push_back(info);
-    }
-
-    for (const auto& symbolInfo : symbols) {
-        std::cout << "Symbol: " << symbolInfo.symbol << std::endl;
-        std::cout << "Quote Asset: " << symbolInfo.quoteAsset << std::endl;
-        std::cout << "Status: " << symbolInfo.status << std::endl;
-        std::cout << "Tick Size: " << symbolInfo.tickSize << std::endl;
-        std::cout << "Step Size: " << symbolInfo.stepSize << std::endl;
-        std::cout << "---------------------" << std::endl;
-    }
-    std::cout << symbols.size() << std::endl; 
+    std::thread spotThread (spotData, spot_exchange_info_base_uri, spot_exchange_info_uri);
+    std::thread usdFutureThread (usdFutureData, usd_futures_exchange_info_base_uri, usd_futures_exchange_info_uri);
+    std::thread coinFutureThread (coinFutureData, coin_futures_exchange_info_base_uri, coin_futures_exchange_info_uri);
+ 
+    spotThread.join();
+    usdFutureThread.join();
+    coinFutureThread.join();
     return 0;
 }
 
---
+++ src/getApiData.cpp
@@ -0,0 +1,136 @@
+
+#include "https.h"
+#include <rapidjson/document.h>
+
+struct symbolInfo{
+    std::string symbol;
+    std::string quoteAsset;
+    std::string status;
+    std::string tickSize;
+    std::string stepSize;
+};
+
+void spotData(std::string spotBaseUrl, std::string spotEndpoint) {
+    auto const host = spotBaseUrl.c_str();
+    auto const port = "443";
+    auto const target = spotEndpoint.c_str();
+    int version = 11;
+    
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // The SSL context is required, and holds certificates
+    ssl::context ctx{ssl::context::tlsv12_client};
+
+    // This holds the root certificate used for verification
+    load_root_certificates(ctx);
+
+    // Verify the remote server's certificate
+    ctx.set_verify_mode(ssl::verify_peer);
+
+    // Launch the asynchronous operation
+    // The session is constructed with a strand to
+    // ensure that handlers do not execute concurrently.
+    std::make_shared<session>(
+        net::make_strand(ioc),
+        ctx
+        )->run(host, port, target, version);
+
+    // Run the I/O service. The call will return when
+    // the get operation is complete.
+    ioc.run();
+
+    http::response<http::string_body> spotExchange;
+    spotExchange = returnResponse();
+    //std::cout << spotExchange << std::endl;
+
+    rapidjson::Document fullData;
+    fullData.Parse(spotExchange.body().c_str());
+
+    if (!fullData.IsObject() || !fullData.HasMember("symbols") || !fullData["symbols"].IsArray()) {
+        std::cerr << "Invalid JSON format or missing symbols array." << std::endl;
+    }
+
+    const auto& symbolsArray = fullData["symbols"];
+    std::vector<symbolInfo> symbols;
+
+    for (const auto& symbol : symbolsArray.GetArray()) {
+        symbolInfo info;
+        info.symbol = symbol["symbol"].GetString();
+        info.quoteAsset = symbol["quoteAsset"].GetString();
+        info.status = symbol["status"].GetString();
+
+        for (const auto& filter : symbol["filters"].GetArray()) {
+            std::string filterType = filter["filterType"].GetString();
+            if (filterType == "PRICE_FILTER") {
+                info.tickSize = filter["tickSize"].GetString();
+            } else if (filterType == "LOT_SIZE") {
+                info.stepSize = filter["stepSize"].GetString();
+            }
+        }
+
+        symbols.push_back(info);
+    }
+
+    for (const auto& symbolInfo : symbols) {
+        std::cout << "Symbol: " << symbolInfo.symbol << std::endl;
+        std::cout << "Quote Asset: " << symbolInfo.quoteAsset << std::endl;
+        std::cout << "Status: " << symbolInfo.status << std::endl;
+        std::cout << "Tick Size: " << symbolInfo.tickSize << std::endl;
+        std::cout << "Step Size: " << symbolInfo.stepSize << std::endl;
+        std::cout << "---------------------" << std::endl;
+    }
+    std::cout << symbols.size() << std::endl; 
+}
+
+void usdFutureData(std::string usdFutureBaseUrl, std::string usdFutureEndpoint) {
+    auto const host = usdFutureBaseUrl.c_str();
+    auto const port = "443";
+    auto const target = usdFutureEndpoint.c_str();
+    int version = 11;
+
+    net::io_context ioc;
+
+    ssl::context ctx{ssl::context::tlsv12_client};
+
+    load_root_certificates(ctx);
+
+    ctx.set_verify_mode(ssl::verify_peer);
+
+    std::make_shared<session>(
+        net::make_strand(ioc),
+        ctx
+        )->run(host, port, target, version);
+
+    ioc.run();
+
+    http::response<http::string_body> usdFutureExchange;
+    usdFutureExchange = returnResponse();
+    std::cout << usdFutureExchange << std::endl;
+}
+
+void coinFutureData(std::string coinFutureBaseUrl, std::string coinFutureEndpoint){
+        auto const host = coinFutureBaseUrl.c_str();
+    auto const port = "443";
+    auto const target = coinFutureEndpoint.c_str();
+    int version = 11;
+
+    net::io_context ioc;
+
+    ssl::context ctx{ssl::context::tlsv12_client};
+
+    load_root_certificates(ctx);
+
+    ctx.set_verify_mode(ssl::verify_peer);
+
+    std::make_shared<session>(
+        net::make_strand(ioc),
+        ctx
+        )->run(host, port, target, version);
+
+    ioc.run();
+
+    http::response<http::string_body> coinFutureExchange;
+    coinFutureExchange = returnResponse();
+    std::cout << coinFutureExchange << std::endl;
+}
