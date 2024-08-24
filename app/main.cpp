21fa16d3625821d4b49565fffdab53bbcde52f69
omer-io <omer.jamil@rakurai.io>
2024-08-24 23:16:42 +0500

    logs wip

--- app/main.cpp
+++ app/main.cpp
@@ -7,6 +7,9 @@
 #include "rapidjson/filereadstream.h"
 #include "boost/asio.hpp"
 #include "boost/bind/bind.hpp"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
 
 // functions declared here are defined in src/getApiData.cpp
 void spotData(std::string, std::string);
@@ -17,7 +20,7 @@ void readQuery();
 // global variables to store base url and endpoints info
 std::string spotExchangeBaseUrl, usdFutureExchangeBaseUrl, coinFutureExchangeBaseUrl;
 std::string spotExchangeEndpoint, usdFutureEndpoint, coinFutureEndpoint;
-int request_interval;
+int requestInterval;
 
 // struct to store logging info from config.json
 struct logsInfo {
@@ -27,7 +30,7 @@ struct logsInfo {
 } logsConfig;
 
 // read config.json for logging, request url, request interval
-void readConfig(std::string configFile, rapidjson::Document &doc) {
+void readConfig(std::string configFile) {
 
     // open config.json
     FILE* fp = fopen(configFile.c_str(), "r"); 
@@ -36,6 +39,8 @@ void readConfig(std::string configFile, rapidjson::Document &doc) {
     if (!fp) { 
         std::cerr << "Error: unable to open file" << std::endl; 
     } 
+
+    rapidjson::Document doc;
 
     char buffer[65536];
     rapidjson::FileReadStream is(fp, buffer, sizeof(buffer));
@@ -52,7 +57,7 @@ void readConfig(std::string configFile, rapidjson::Document &doc) {
     spotExchangeEndpoint = doc["exchange_endpoints"]["spot_exchange_info_uri"].GetString();
     usdFutureEndpoint = doc["exchange_endpoints"]["usd_futures_exchange_info_uri"].GetString();
     coinFutureEndpoint = doc["exchange_endpoints"]["coin_futures_exchange_info_uri"].GetString();
-    request_interval = doc["request_interval"].GetInt();
+    requestInterval = doc["request_interval"].GetInt();
     
     // store logging level, file enable, console enable
     logsConfig.level = doc["logging"]["level"].GetString();
@@ -80,6 +85,35 @@ void fetchAll(const boost::system::error_code& /*e*/, boost::asio::steady_timer*
     
 }
 
+void setSpdLogs(){
+
+    // Create a vector of sinks
+    std::vector<spdlog::sink_ptr> sinks;
+
+    // Add console sink if enabled
+    if (logsConfig.console) {
+        sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    }
+
+    // Add file sink if enabled
+    if (logsConfig.file) {
+        sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/logfile.log", true));
+    }
+
+    // Create combined logger
+    auto logger = std::make_shared<spdlog::logger>("multi_sink", begin(sinks), end(sinks));
+
+    // set level
+    logger->set_level(spdlog::level::from_str(logsConfig.level));
+    
+    // register logger
+    spdlog::register_logger(logger);
+    spdlog::set_default_logger(logger);
+    spdlog::flush_every(std::chrono::seconds(1));
+
+}
+
+
 // run ioc.run() in a thread
 // void runIoContext(boost::asio::io_context& io) {
 //     io.run();
@@ -87,20 +121,27 @@ void fetchAll(const boost::system::error_code& /*e*/, boost::asio::steady_timer*
 
 int main() {
 
-    rapidjson::Document doc;
-    readConfig("/home/omer/training/BinanceExchangeHandler/config.json", doc);
-
+    readConfig("/home/omer/training/BinanceExchangeHandler/config.json");
+    setSpdLogs();
     std::cout << "Spot Exchange Info base URI: " << spotExchangeBaseUrl << std::endl;
     std::cout << "USD Futures Exchange Info base URI: " << usdFutureExchangeBaseUrl << std::endl;
     std::cout << "Coin Futures Exchange Info base URI: " << coinFutureExchangeBaseUrl << std::endl;
     std::cout << "Spot Exchange Info URI: " << spotExchangeEndpoint << std::endl;
     std::cout << "USD Futures Exchange Info URI: " << usdFutureEndpoint << std::endl;
     std::cout << "Coin Futures Exchange Info URI: " << coinFutureEndpoint << std::endl;
-    std::cout << "Request Interval: " << request_interval << " seconds" << std::endl;
+    std::cout << "Request Interval: " << requestInterval << " seconds" << std::endl;
 
     std::cout << "Logging Level: " << logsConfig.level << std::endl;
     std::cout << "Logging to File: " << (logsConfig.file ? "Enabled" : "Disabled") << std::endl;
     std::cout << "Logging to Console: " << (logsConfig.console ? "Enabled" : "Disabled") << std::endl;
+
+    spdlog::info("Spot Exchange Info base URI: {}", spotExchangeBaseUrl);
+    spdlog::info("USD Futures Exchange Info base URI: {}", usdFutureExchangeBaseUrl);
+    spdlog::info("Coin Futures Exchange Info base URI: {}", coinFutureExchangeBaseUrl);
+    spdlog::info("Spot Exchange Info URI: {}", spotExchangeEndpoint);
+    spdlog::info("USD Futures Exchange Info URI: {}", usdFutureEndpoint);
+    spdlog::info("Coin Futures Exchange Info URI: {}", coinFutureEndpoint);
+    spdlog::info("Request Interval: {} seconds", requestInterval);
 
     std::thread readQueryThread(readQuery);
 
